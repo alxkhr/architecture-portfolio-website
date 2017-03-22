5a09f0907cff4f20211975ec1337c1e737c665bc
Alexander Kehr <alexander.kehr@mobilexag.de>
2017-03-22 19:09:08 +0100

    testing with ava and enzyme :white_check_mark:

---
+++ test/comp.test.js
@@ -0,0 +1,11 @@
+import test from 'ava';
+import Comp from '../src/comp';
+
+test('foo', t => {
+  t.pass();
+});
+
+test('bar', async t => {
+  const bar = Promise.resolve('bar');
+  t.is(await bar, 'bar');
+});
