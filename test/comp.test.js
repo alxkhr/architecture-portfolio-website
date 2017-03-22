658c266ab584374a5d5ded0bf414cf3539a8ab59
Alexander Kehr <alexander.kehr@mobilexag.de>
2017-03-22 21:23:50 +0100

    mock css-modules and that bad global var for testing :white_check_mark:

--- test/comp.test.js
+++ test/comp.test.js
@@ -1,7 +1,31 @@
+import React from 'react';
 import test from 'ava';
+import { shallow } from 'enzyme';
 import Comp from '../src/comp';
 
+global.rsscContent = {
+  title: 'foo title',
+  projects: [
+    {
+      year: '2001',
+      title: 'some title',
+      imageSource: 'example-image.jpg',
+      thumbnailSource: 'example-thumb.jpg',
+      description: 'some *description* about the project',
+    },
+    {
+      year: '2007',
+      title: 'some other title',
+      imageSource: 'example-image.jpg',
+      thumbnailSource: 'example-thumb.jpg',
+      description: 'some other description about the project',
+    },
+  ],
+};
+
 test('foo', t => {
+  const wrapper = shallow(<Comp />);
+  console.log('comp', wrapper.html());
   t.pass();
 });
 
